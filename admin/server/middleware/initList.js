031afbc613571fdacbfe3b10421a48dbea262645
financiali <ali@freelancer.run>
2019-07-22 18:03:06 +0100

    - User module security

--- admin/server/middleware/initList.js
+++ admin/server/middleware/initList.js
@@ -1,12 +1,21 @@
-module.exports = function initList (req, res, next) {
+module.exports = function initList(req, res, next) {
 	var keystone = req.keystone;
 	req.list = keystone.list(req.params.list);
+
 	if (!req.list) {
 		if (req.headers.accept === 'application/json') {
-			return res.status(404).json({ error: 'invalid list path' });
+			return res.status(404).json({error: 'invalid list path'});
 		}
 		req.flash('error', 'List ' + req.params.list + ' could not be found.');
 		return res.redirect('/' + keystone.get('admin path'));
+	} else {
+
+		if (!req.user.modulesList.includes(req.list.key)) {
+			// return res.status(501).json({error: 'invalid list path'});
+			next(new Error('test'));
+		} else {
+			next();
+		}
 	}
-	next();
+
 };
