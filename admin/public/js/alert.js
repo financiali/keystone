6a5f892462a5630861831e1150393ee40742e9b2
financiali <ali@freelancer.run>
2019-08-12 11:37:56 +0100

    - Socket UI

--- admin/public/js/alert.js
+++ admin/public/js/alert.js
@@ -10,6 +10,7 @@ $(document).ready(function () {
 		$('.alert-container .alert-message').html('');
 		if (soundFile) {
 			soundFile.pause()
+			soundFile = null;
 		}
 	};
 
@@ -19,7 +20,7 @@ $(document).ready(function () {
 		var bg_color = data.bg_color || '#2b2e2b';
 		var timeout = data.timeout || 15000;
 
-		if (typeof data.sound !== "undefined") {
+		if (typeof data.sound !== "undefined" && data.sound && data.sound.length > 1) {
 			soundFile = new Pizzicato.Sound(data.sound, function () {
 				soundFile.play();
 			});
@@ -35,7 +36,7 @@ $(document).ready(function () {
 			clear();
 		}, timeout);
 
-		if (type === 'alert' && typeof message!=="undefined" && message.length>0) {
+		if (type === 'alert' && typeof message !== "undefined" && message.length > 0) {
 			$('body').addClass('alert-state');
 			$('.alert-container').addClass('show');
 			$('.alert-container .alert-message').html(message);
